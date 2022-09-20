ee0534a2cf178c3f80ba364e4b4140559f2a1242
fitChan <84306157+fitChan@users.noreply.github.com>
2022-09-20 17:24:58 +0900

    authentication Manager apply

--- src/main/java/com/miniproject/userservice/security/WebSecurity.java
+++ src/main/java/com/miniproject/userservice/security/WebSecurity.java
@@ -1,24 +1,53 @@
 package com.miniproject.userservice.security;
 
+import com.miniproject.userservice.service.AuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
-public class WebSecurity{
+public class WebSecurity {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/user-service/**").permitAll();
+
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("39.115.182.8")
+                .and()
+                .addFilter(getAthenticationFilter());
+
+
         http.headers().frameOptions().disable();
 
         return http.build();
+    }
+
+    private AuthenticationFilter getAthenticationFilter() throws Exception{
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        AuthenticationConfiguration authenticationConfiguration = new AuthenticationConfiguration();
+
+        authenticationFilter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return authenticationFilter;
     }
 
 
@@ -27,4 +56,8 @@ public class WebSecurity{
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**","/h2/**");
     }
+
+
+
+
 }
